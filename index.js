91fd7268af83c102c20db5d8279182947960930a
oiiemon9 <oiiemon9@gmail.com>
2025-11-09 15:42:29 +0600

    top study partners get complete

--- index.js
+++ index.js
@@ -41,6 +41,15 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/top-study-partners', async (req, res) => {
+      const result = await partnerProfilesCollection
+        .find()
+        .sort({ rating: -1 })
+        .limit(4)
+        .toArray();
+      res.send(result);
+    });
+
     await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
